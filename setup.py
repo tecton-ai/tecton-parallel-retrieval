3ea5af74e3fd690c1b1cc87780a4708c39da2195
srydzewski <samantha@tecton.ai>
2024-02-29 12:38:11 -0800

    0.0.16

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup
 
-setup(name='tecton_parallel_retrieval',
-            version='0.0.15',
-            description='[private preview] Parallel feature retrieval on Databricks for Tecton',
-            author='Tecton',
-            packages=['tecton_parallel_retrieval'],
-            license="Apache License 2.0",
-            install_requires=[
-                "databricks-sdk",
-                "pyspark",
-                "tecton ==0.8.*",
-            ],
-            setup_requires=["setuptools", "wheel"],
-            url="https://tecton.ai",
-            python_requires=">=3.7",
-           )
+setup(
+    name='tecton_parallel_retrieval',
+    version='0.0.16',
+    description='[private preview] Parallel feature retrieval on Databricks for Tecton',
+    author='Tecton',
+    packages=['tecton_parallel_retrieval'],
+    license="Apache License 2.0",
+    install_requires=[
+        "databricks-sdk",
+        "pyspark",
+        "tecton ==0.8.*",
+    ],
+    setup_requires=["setuptools", "wheel"],
+    url="https://tecton.ai",
+    python_requires=">=3.7",
+)
