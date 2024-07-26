3987c00cbf5635eb966f5763d2b4e232d7ee1c3e
zhongyangfu <zyf@tecton.ai>
2024-07-26 13:44:29 -0400

    fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.17-beta.1',
     description='[private preview] Parallel feature retrieval for Tecton',
     author='Tecton',
-    packages=['tecton_parallel_retrieval'],
+    packages=['tecton_parallel_retrieval', 'tecton_parallel_retrieval.spark', 'tecton_parallel_retrieval.rift'],
     license="Apache License 2.0",
     install_requires=[
         "tecton>=0.9,<0.10",
